41a249a66eaea5c05ee5006fc26c0cd8b95b2e50
Mihir Gupta <mihirgupta665@gmail.com>
2025-11-26 08:02:02 +0530

    Creating a Edit Api and Implementing it

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(express.static(path.join(__dirname, "view")));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended : true}));
 app.use(methodOverride("_method"));
 
@@ -61,6 +61,7 @@ app.get("/listings", async (req, res)=>{
 app.get("/listings/new", (req,res)=>{
     res.render("listings/new.ejs");
 });
+// post listing
 app.post("/listings", async (req,res)=>{
     let {listing} = req.body;
     let newlisting = await new Listing(listing);
@@ -74,5 +75,12 @@ app.get("/listings/:id", async (req, res)=>{
     let listing  = await Listing.findById(id);
     // console.log(listing);
     res.render("listings/show.ejs", {listing});
+})
+
+app.get("/listings/:id/edit", async (req, res)=>{
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
+    console.log(listing);
+    res.render("listings/edit.ejs", { listing });
 })
 
