d0b518709b0b2652c7e962de247b63366b098419
Mihir Gupta <mihirgupta665@gmail.com>
2025-12-01 09:57:32 +0530

    Creating Custom Express Error and implementing it to all wrong route

--- app.js
+++ app.js
@@ -5,107 +5,119 @@ const path = require("path");
 const Listing = require("./models/listing.js");
 const ejsMate = require("ejs-mate");    // ejs-mate is used to create a styled template
 const asyncWrap = require("./utility/asyncWrap.js");
+const ExpressError = require("./utility/ExpressError.js");
 
 const app = express();
 
-app.set("views", path.join(__dirname, "views"));
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended : true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
-app.listen(8080, ()=>{
-    console.log("Listening through port  : "+8080);
+app.listen(8080, () => {
+    console.log("Listening through port  : " + 8080);
 })
 
-app.engine("ejs", ejsMate);
-
-async function main(){
+async function main() {
     await mongoose.connect("mongodb://127.0.0.1:27017/wonderland");
 }
 
-main().then((res)=>{
+main().then((res) => {
     console.log("Connection Established");
-}).catch((err)=>{
-    console.log("Error in connecting mongodb database : "+err);
+}).catch((err) => {
+    console.log("Error in connecting mongodb database : " + err);
 });
 
 // home api
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
     res.send("Server is Working Fine!!!");
 })
 
 // testing api
-app.get("/listingTest", (req,res)=>{
+app.get("/listingTest", (req, res) => {
     let testList = new Listing({
-        title : "My testing Villa",
-        description : "12 BHK with swimming pool",
-        price : 12000,
-        location : "Jalandhar",
+        title: "My testing Villa",
+        description: "12 BHK with swimming pool",
+        price: 12000,
+        location: "Jalandhar",
         country: "India"
     });
-    testList.save().then((res)=>{
+    testList.save().then((res) => {
         console.log(res);
-    }).catch((err)=>{
-        console.log("Error in inserting testing villa to database : "+err);
+    }).catch((err) => {
+        console.log("Error in inserting testing villa to database : " + err);
     });
     // sample data insterted successfuly so res.send function is used at last to display the message
-    res.send("Sucess!!  : Testing Sample Data inserted into database ");   
+    res.send("Sucess!!  : Testing Sample Data inserted into database ");
 });
 
 // index api
-app.get("/listings", async (req, res)=>{
+app.get("/listings", asyncWrap(async (req, res) => {
     let listings = await Listing.find({});
     // as veiw has been set therefore express will find vies directory and specified file will be rendered as ejs by default.
-    res.render("listings/index.ejs", {listings});    
-})
+    res.render("listings/index.ejs", { listings });
+}));
 
 // new listing
-app.get("/listings/new", (req,res)=>{
+app.get("/listings/new", (req, res) => {
     res.render("listings/new.ejs");
 });
 // post listing
-app.post("/listings", asyncWrap(async (req,res)=>{
-    let {listing} = req.body;
+app.post("/listings", asyncWrap(async (req, res, next) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "The Listing for insertion is Empty");
+    }
+    let { listing } = req.body;
     let newlisting = await new Listing(listing);
     await newlisting.save();
     res.redirect("/listings");
 }));
 
 // Read or Show Api
-app.get("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
-    let listing  = await Listing.findById(id);
+app.get("/listings/:id", asyncWrap(async (req, res) => {
+    let { id } = req.params;
+    let listing = await Listing.findById(id);
     // console.log(listing);
-    res.render("listings/show.ejs", {listing});
-})
+    res.render("listings/show.ejs", { listing });
+}));
 
-app.get("/listings/:id/edit", async (req, res)=>{
+app.get("/listings/:id/edit", asyncWrap(async (req, res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     // console.log(listing);
     res.render("listings/edit.ejs", { listing });
-})
+}));
 
-app.put("/listings/:id", async (req,res)=>{
+app.put("/listings/:id", asyncWrap(async (req, res) => {
+    if(!req.body.listing){
+        throw new ExpressError(400, "Please Enter a Valid Updated Listings");
+    }
     let { id } = req.params;
     //with destructing of object its properties becomes direct keys and value
-    console.log(req.body.listing)
+    // console.log(req.body.listing)
     await Listing.findByIdAndUpdate(id, req.body.listing)   //deconstructing the listing object using ...req.body.listing
     res.redirect(`/listings/${id}`);
-});
+}));
 
-app.delete("/listings/:id", async (req, res)=>{
-    let {id} = req.params;
+app.delete("/listings/:id", asyncWrap(async (req, res) => {
+    let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing);
+    // console.log(deletedListing);
     res.redirect("/listings");
+}));
+
+app.use((req, res, next)=>{
+    next(new ExpressError(404, "Page Not Found"));
 });
 
-app.use((err, req, res, next) => {  
+app.use((err, req, res, next) => {
+    let { status = 500, message = "Somthing Went Wrong!" } = err;
+    res.status(status).render("error", {err} );
     console.log(err.name);
-    res.send(err.name);
+
 });
 
 // Form Validation : When we enter the data iun the form the browser must check whether the data is properly formated and obeys all the constraint set by the application.
