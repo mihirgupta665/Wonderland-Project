8856ab8f412a854f3feae43c0917252fb3aeb013
Mihir Gupta <mihirgupta665@gmail.com>
2025-12-16 06:55:13 +0530

    Implementing Marker features to the map

--- public/js/map.js
+++ public/js/map.js
@@ -1,11 +1,16 @@
 
-    // let mapToken = mapToken; 
-    console.log(mapToken); 
+// let mapToken = mapToken; 
+// console.log(mapToken);
 
-    /* Map_Token is accessible in ejs file only so we have define maptoken variable containing the value of map.token */
-    mapboxgl.accessToken = mapToken;        
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-    center: [81.0004, 26.9070], // starting position [lng, lat]. Note that lat must be set between -90 and 90
-    zoom: 9 // starting zoom
-            });
+/* Map_Token is accessible in ejs file only so we have define maptoken variable containing the value of map.token */
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+    zoom: 10 // starting zoom
+});
+
+console.log("Coordinates", coordinates);
+const marker1 = new mapboxgl.Marker({color: "red"})
+    .setLngLat(coordinates)
+    .addTo(map);
